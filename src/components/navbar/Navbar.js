4627b265a9afaa8eecabc768e6c9efc55ac12648
herold1989 <jens_herold@gmx.net>
2022-07-18 23:36:09 +0200

    add mobile-menu to navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { HiOutlineMenuAlt4 } from 'react-icons/hi'
+import { AiOutlineClose } from 'react-icons/ai'
+import { FaInstagram } from 'react-icons/fa'
+
+
 
 import './NavbarStyles.css'
 
 function Navbar() {
+
+    const [nav, setNav] = useState(false)
+    const handleNav = () => setNav(!nav)
+
     return (
         <div className="navbar">
 
@@ -17,6 +26,33 @@ function Navbar() {
                 <li>BOOK A VISIT</li>
                 <li>CONTACT</li>
             </ul>
+
+            <div className="cellular">CALL ME: +1234-567-8910</div>
+
+            {/* <div className='hamburger' onClick={handleNav}>
+                 <HiOutlineMenuAlt4 className='icon' />
+             </div> */}
+            <div className='hamburger' onClick={handleNav}>
+                {!nav ? (<HiOutlineMenuAlt4 className='icon' />) : <AiOutlineClose className='icon' />}
+            </div>
+
+            <div className={nav ? 'mobile-menu active' : 'mobile-menu'}>
+                <ul className="mobile-nav">
+                    <li>HOME</li>
+                    <li>DEVELOPING</li>
+                    <li>GALLERY</li>
+                    <li>BOOK A VISIT</li>
+                    <li>CONTACT</li>
+                </ul>
+                <div className='mobile-menu-bottom'>
+                    <h4>More of our exciting work:</h4>
+                    <div className='social-icons'>
+                        <a href="https://www.instagram.com/esteves.inc/">
+                        <FaInstagram className='icon'/>
+                        </a>
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
