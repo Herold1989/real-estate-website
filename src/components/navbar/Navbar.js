67ce273d6523642541a4f557b5fd5a9f8ce5783a
herold1989 <jens_herold@gmx.net>
2022-07-19 16:58:26 +0200

    further navbar alterations

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { HiOutlineMenuAlt4 } from 'react-icons/hi'
-import { AiOutlineClose } from 'react-icons/ai'
+import { AiOutlineClose, AiOutlinePhone} from 'react-icons/ai'
 import { FaInstagram } from 'react-icons/fa'
 
 
@@ -25,11 +25,16 @@ function Navbar() {
                 <li>BOOK A VISIT</li>
                 <li>CONTACT</li>
             </ul>
+            <div className='nav-icons'>
+                    <select className='icon' style={{marginRight: '1rem'}}>
+                        <option value="1">EN</option>
+                        <option value="2">PT</option>
+                    </select>
+                <AiOutlinePhone className='icon' />
+            </div>
 
-            <div className="cellular">CALL ME: +1234-567-8910</div>
-            
             <div className={nav ? 'hamburger dark' : 'hamburger'} onClick={handleNav}>
-                {!nav ? (<HiOutlineMenuAlt4 className='icon' />): <AiOutlineClose className='icon' />}
+                {!nav ? (<HiOutlineMenuAlt4 className='icon' />) : <AiOutlineClose className='icon' />}
             </div>
 
             <div className={nav ? 'mobile-menu active' : 'mobile-menu'}>
@@ -44,7 +49,7 @@ function Navbar() {
                     <h4>More of our exciting work:</h4>
                     <div className='social-icons'>
                         <a href="https://www.instagram.com/esteves.inc/">
-                        <FaInstagram className='icon'/>
+                            <FaInstagram className='icon' />
                         </a>
                     </div>
                 </div>
