27e3f3f72947d4fb5f9baea620d0ef7c0f12c3ba
herold1989 <jens_herold@gmx.net>
2022-07-20 13:07:13 +0200

    start with about section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import About from "./components/about/About";
 import Developing from "./components/developing/Developing";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
@@ -9,6 +10,7 @@ function App() {
      <Navbar/>
      <Hero/>
      <Developing/>
+     <About/>
     </div>
   );
 }
