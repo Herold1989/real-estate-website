c703406b352df9a97c9109d0f038e3050993b321
herold1989 <jens_herold@gmx.net>
2022-07-30 12:12:23 +0200

    added a very basic footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from "./components/about/About";
 import Developing from "./components/developing/Developing";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
      <Hero/>
      <Developing/>
      <About/>
+     <Footer/>
     </div>
   );
 }
