ae111ba0937381f3fb86d2f7434714d206f80184
herold1989 <jens_herold@gmx.net>
2022-09-03 17:00:43 +0200

    adding links (react-scroll) to navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
 import { HiOutlineMenuAlt4 } from 'react-icons/hi'
-import { AiOutlineClose, AiOutlinePhone} from 'react-icons/ai'
+import { AiOutlineClose, AiOutlinePhone } from 'react-icons/ai'
 import { FaInstagram } from 'react-icons/fa'
 
-import {Link} from 'react-scroll'
+import { Link } from 'react-scroll'
 
 import './NavbarStyles.css'
 
@@ -19,20 +19,20 @@ function Navbar() {
             </div>
 
             <ul className="nav-menu">
-            <Link to='home' smooth ={true} duration={500}> <li>HOME</li></Link>
-            <Link to='developing' smooth ={true} duration={500}> <li>DEVELOPING</li></Link>
-            <Link to='gallery' smooth ={true} duration={500}> <li>GALLERY</li></Link>
-            <Link to='form' smooth ={true} duration={500}> <li>BOOK A VISIT</li></Link>
-            <Link to='footer' smooth ={true} duration={500}> <li>CONTACT</li></Link>
+                <Link to='home' smooth={true} duration={500}> <li>HOME</li></Link>
+                <Link to='developing' smooth={true} duration={500}> <li>DEVELOPING</li></Link>
+                <Link to='gallery' smooth={true} duration={500}> <li>GALLERY</li></Link>
+                <Link to='form' smooth={true} duration={500}> <li>BOOK A VISIT</li></Link>
+                <Link to='footer' smooth={true} duration={500}> <li>CONTACT</li></Link>
             </ul>
             <div className='nav-icons'>
-                    <select className='icon' style={{marginRight: '1rem'}}>
-                        <option value="1">EN</option>
-                        <option value="2">PT</option>
-                    </select>
-                    <a href="tel: +1-234-567-8910">
-                            <AiOutlinePhone className='icon'/>
-                    </a>
+                <select className='icon' style={{ marginRight: '1rem' }}>
+                    <option value="1">EN</option>
+                    <option value="2">PT</option>
+                </select>
+                <a href="tel: +1-234-567-8910">
+                    <AiOutlinePhone className='icon' />
+                </a>
             </div>
 
             <div className={nav ? 'hamburger dark' : 'hamburger'} onClick={handleNav}>
@@ -41,11 +41,11 @@ function Navbar() {
 
             <div className={nav ? 'mobile-menu active' : 'mobile-menu'}>
                 <ul className="mobile-nav">
-                    <li>HOME</li>
-                    <li>DEVELOPING</li>
-                    <li>GALLERY</li>
-                    <li>BOOK A VISIT</li>
-                    <li>CONTACT</li>
+                <Link to='hero' smooth ={true} duration={500}> <li>HOME</li></Link>
+                <Link to='developing' smooth ={true} duration={500}> <li>DEVELOPING</li></Link>
+                <Link to='gallery' smooth ={true} duration={500}> <li>GALLERY</li></Link>
+                <Link to='contact' smooth ={true} duration={500}> <li>BOOK A VISIT</li></Link>
+                <Link to='footer' smooth ={true} duration={500}> <li>CONTACT</li></Link> 
                 </ul>
                 <div className='mobile-menu-bottom'>
                     <h4>More of our exciting work:</h4>
